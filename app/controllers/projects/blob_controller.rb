c109e7cdaac91fb20b61576279f30fede6cc0777
qq <qingqing@nfs.iscas.ac.cn>
2015-12-09 16:42:23 +0800

    record add/update project add issue/mergerequest/commit to search interface

--- app/controllers/projects/blob_controller.rb
+++ app/controllers/projects/blob_controller.rb
@@ -2,7 +2,7 @@
 class Projects::BlobController < Projects::ApplicationController
   include ExtractsPath
   include ActionView::Helpers::SanitizeHelper
-
+  include IscasSearchService
   # Raised when given an invalid file path
   class InvalidPathError < StandardError; end
 
@@ -16,19 +16,25 @@ class Projects::BlobController < Projects::ApplicationController
   before_action :require_branch_head, only: [:edit, :update]
   before_action :editor_variables, except: [:show, :preview, :diff]
   before_action :after_edit_path, only: [:edit, :update]
-
+  include IscasSearchService
   def new
     commit unless @repository.empty?
   end
 
   def create
     result = Files::CreateService.new(@project, current_user, @commit_params).execute
-
+    message=@commit_params[:commit_message]
+    projectId=@project.id
+    date=Time.now.strftime("%Y-%m-%dT%H:%M:%S")
+    commit
+    id=@commit.id
+    #iscas_search
+    addCommit(id,message,projectId,date)
     if result[:status] == :success
       flash[:notice] = "Your changes have been successfully committed"
       respond_to do |format|
-        format.html { redirect_to namespace_project_blob_path(@project.namespace, @project, File.join(@target_branch, @file_path)) }
-        format.json { render json: { message: "success", filePath: namespace_project_blob_path(@project.namespace, @project, File.join(@target_branch, @file_path)) } }
+        format.html { redirect_to namespace_project_blob_path(@project.namespace, @project,File.join(@target_branch, @file_path)) }
+        format.json { render json: { message: "success", filePath: namespace_project_blob_path(@project.namespace, @project,File.join(@target_branch, @file_path)) } }
       end
     else
       flash[:alert] = result[:message]
@@ -38,6 +44,7 @@ class Projects::BlobController < Projects::ApplicationController
       end
     end
   end
+ 
 
   def show
   end
@@ -119,7 +126,6 @@ class Projects::BlobController < Projects::ApplicationController
 
   def commit
     @commit = @repository.commit(@ref)
-
     return not_found! unless @commit
   end
 
