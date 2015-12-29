a16de1035346560d8bbedd2d3a243a311d3de334
zhangyuguang <yuguang@nfs.iscas.ac.cn>
2015-12-29 15:53:56 +0800

    solved conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 
 ActiveRecord::Schema.define(version: 20151229033756) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
   
   create_table "abuse_reports", force: true do |t|
     t.integer  "reporter_id"
