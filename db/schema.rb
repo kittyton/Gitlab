0b4f87fa5bfaf0e44eb3c75bfd895aa0e3c17bb6
zhangyuguang <yuguang@nfs.iscas.ac.cn>
2015-12-29 15:53:56 +0800

    solved conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20151229033756) do
-  
+
   create_table "abuse_reports", force: true do |t|
     t.integer  "reporter_id"
     t.integer  "user_id"
