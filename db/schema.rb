46c0d88619d0454c708e74f3268454a54396613f
zhangyuguang <yuguang@nfs.iscas.ac.cn>
2015-12-29 15:53:56 +0800

    add api for addTagPushListener, add a item to identify whether the peoject has a task_id, add iscas for test
    Conflicts:
    	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
-
 ActiveRecord::Schema.define(version: 20151229033756) do
   
   create_table "abuse_reports", force: true do |t|
